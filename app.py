b106986e3f233f8a227946120d134482fb31ca39
Jiamu Liu <jiamu421@ucla.edu>
2022-01-31 01:32:09 -0500

    add base index and view

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import re
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-
 def index():
     return render_template("index.html")
+
+@app.route('/view/')
+def view_records():
+    return render_template('view.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
