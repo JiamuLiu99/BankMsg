3b7de937ed6ba2ea0d85754efa85348fa3d4127d
Jiamu Liu <jiamu421@ucla.edu>
2022-01-31 19:58:26 -0500

    step 1 - add db

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-import re
-from flask import Flask, render_template
+import core
+from flask import Flask, g, render_template, request
+
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    core.get_message_db()
+    if request.method == 'GET':
+        return render_template("submit.html")
+    else:
+        try:
+            core.insert_message(request)
+            return render_template('submit.html', msg=request.form['message'], hdl=request.form['handle'])
+        except:
+            return render_template('error.html')
 
 @app.route('/view/')
 def view_records():
+    core.get_message_db()
     return render_template('view.html')
+
 
 if __name__ == "__main__":
     app.run(debug = True)
